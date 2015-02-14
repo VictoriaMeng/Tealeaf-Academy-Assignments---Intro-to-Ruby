462dc761e3d10abdd45e3f2ad97712c4839b68d5
Victoria Meng <vym205@nyu.edu>
2015-02-14 20:44:13 +0000

    Overhauled OO code in deal method

--- blackjack_oo.rb
+++ blackjack_oo.rb
@@ -8,140 +8,139 @@ end
 class Game
   include Format
   
+  attr_accessor :main_deck, :player, :dealer
+  
   def initialize
-    @deck = Deck.new
+    @main_deck = Deck.new
     @player = Hand.new("Player")
     @dealer = Hand.new("Dealer")
-    full_game(@deck, @player, @dealer)
+    full_game
   end
   
-  def setup(deck, player, dealer)
+  def setup
     2.times do
-      Card.deal(@deck, @player)
-      Card.deal(@deck, @dealer)
+      deal(@player)
+      deal(@dealer)
     end
-    show_hands(player, dealer)
+    show_hands
   end
   
-  def show_hands(player, dealer)
-    p "#{dealer.name} Hand: #{dealer.hand.collect {|card| card[0]}} = #{dealer.hand_total}".tr('"', '')
-    p "#{player.name} Hand: #{player.hand.collect {|card| card[0]}} = #{player.hand_total}".tr('"', '')
+  def deal(name)
+    @card = @main_deck.deck.sample
+    @main_deck.deck.delete_at(@main_deck.deck.index(@card))
+    name.hand << @card
+    name.ace_value
+    puts "Card dealt: #{@card[0]}."
+  end
+  
+  def show_hands
+    p "#{@dealer.name} Hand: #{@dealer.hand.collect {|card| card[0]}} = #{@dealer.hand_total}".tr('"', '')
+    p "#{@player.name} Hand: #{@player.hand.collect {|card| card[0]}} = #{@player.hand_total}".tr('"', '')
   end
 
   def play_again
     say "Press 'Y' to play again! Press any other key to exit."
     @input = gets.chomp
     if @input.downcase == "y" || @input.downcase == "yes"
-      game = Game.new
+      Game.new
     else 
       puts "Thanks for playing!"
     end
   end
   
-  def blackjack_win(player, dealer)
-    if player.hand_total == 21
+  def blackjack_win
+    if @player.hand_total == 21
       say "Blackjack! You win!"
-      @blackjack_win = "true"
-    elsif dealer.hand_total == 21
+      true
+    elsif @dealer.hand_total == 21
       say "Dealer gets Blackjack! You lose!"
-      @blackjack_win = "true"
-    elsif player.hand_total != 21 && dealer.hand_total != 21
-      say "No one gets Blackjack. Game continues." 
-      @blackjack_win = "false"
+      true
+    elsif @player.hand_total != 21 && @dealer.hand_total != 21
+      false
     end
   end
 
-  def check_winner(player, dealer)
-    show_hands(player, dealer)
-    if player.hand_total > dealer.hand_total
+  def check_winner
+    say "Final Hands"
+    show_hands
+    if @player.hand_total > @dealer.hand_total
       say "You win!"
-    else
-      player.hand_total <= dealer.hand_total
+    elsif @player.hand_total <= @dealer.hand_total
       say "You lose!"
     end
   end
   
-  def player_turn(deck, player, dealer)
+  def player_turn
     say "Your Turn"
     say "Hit or Stand? (H/S)"
     @input = gets.chomp
-    hit_or_stand(deck, player, dealer)
-    player.bust_check
-    show_hands(player, dealer) if player.bust == "false"
-    player_turn(deck, player, dealer) if player.bust == "false" && @stand != "true" 
+    hit_or_stand
+    @player.bust_check
+    show_hands if !@player.bust
+    player_turn if !@player.bust && !@stand
   end
 
-  def hit_or_stand(deck, player, dealer)
+  def hit_or_stand
     if @input.downcase == "h" || @input.downcase == "hit"
-      Card.deal(deck, player) 
+      deal(@player)
     elsif @input.downcase == "s" || @input.downcase == "stand"
-      @stand = "true"
+      @stand = true
     else
       say "Please enter 'H' for 'Hit', or 'S' for 'Stand'."
-      player_turn(deck, player, dealer)
+      player_turn
     end
   end
 
-  def dealer_turn(deck, player, dealer)
+  def dealer_turn
     say "Dealer Turn"
-    until dealer.hand_total >= 17
-      show_hands(player, dealer)
-      Card.deal(deck, dealer)
+    until @dealer.hand_total >= 17
+      show_hands
+      deal(@dealer)
     end
-    dealer.bust_check
+    @dealer.bust_check
+    puts "Dealer stands." unless @dealer.bust
   end
   
-  def full_game(deck, player, dealer)
-    setup(deck, player, dealer)
-    if blackjack_win(player, dealer) == "true"
+  def full_game
+    setup
+    if blackjack_win
       play_again
     else
-      player_turn(deck, player, dealer)
-      dealer_turn(deck, player, dealer) if player.bust == "false"
-      check_winner(player, dealer) if dealer.bust == "false" && player.bust == "false"
+      player_turn
+      dealer_turn if !@player.bust
+      check_winner if !@dealer.bust && !@player.bust
       play_again
     end
   end
   
 end
 
-class Card  
+class Deck
   include Format
   
-  attr_accessor :name, :deck, :hand, :card, :hand_total, :bust
-  
-  def self.deal(deck, hand)
-    @card = deck.deck.sample
-    deck.deck.delete_at(deck.deck.index(@card))
-    hand.hand << @card
-    hand.ace_value
-    puts "Card dealt: #{@card[0]}."
-  end
- 
-end
-
-class Deck < Card
-  include Format
+  attr_accessor :deck, :card, :hand
   
   def initialize
     @deck = []
     4.times do 
       for integer in 2..10
-        self.deck << [integer, integer]
+        @deck << [integer, integer]
       end
-      self.deck.push(['Jack', 10], ['Queen', 10], ['King', 10], ['Ace(11)', 11])
+      @deck.push(['Jack', 10], ['Queen', 10], ['King', 10], ['Ace(11)', 11])
     end
   end
   
 end
 
-class Hand < Card
+class Hand
   include Format
+  
+  attr_accessor :name, :hand, :hand_total, :bust
   
   def initialize(name)
     @name = name
     @hand = []
-    @bust = "false"
+    @bust = false
   end
   
   def hand_total
@@ -151,17 +150,19 @@ class Hand < Card
   end
   
   def ace_value
-    self.hand[(self.hand.index(['Ace(11)', 11]))] = ['Ace(1)', 1] if hand_total > 21 && self.hand.include?(['Ace(11)', 11])
+    if hand_total > 21 && @hand.include?(['Ace(11)', 11])
+      @hand[(@hand.index(['Ace(11)', 11]))] = ['Ace(1)', 1] 
+    end
   end
   
   def bust_check
-    if self.hand_total > 21
-      p "#{self.name} Hand: #{self.hand.collect {|card| card[0]}} = #{self.hand_total}".tr('"', '')
-      say "That's over 21! #{self.name} busted!"
-      @bust = "true"
+    if hand_total > 21
+      p "#{@name} Hand: #{@hand.collect {|card| card[0]}} = #{@hand_total}".tr('"', '')
+      say "That's over 21! #{@name} busted!"
+      @bust = true
     end
   end
 
 end
 
-game = Game.new
+Game.new
